765f70166bed9b91c0bd1844158427d605506a3d
Mauro-Filho <michelgoiania2020@gmail.com>
2025-01-22 14:15:27 -0300

    feat: usuarios integração

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -3,6 +3,8 @@ import "@fastify/jwt";
 declare module "@fastify/jwt" {
   export interface FastifyJWT {
     user: {
+      name: any;
+      userName: any;
       id: any;
       role: "ADMIN" | "MEMBER" | "WALLET" | "GRAPHIC";
       type: string;
