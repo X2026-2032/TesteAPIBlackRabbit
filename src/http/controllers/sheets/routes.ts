f571d69414cbe24f3104b6cc2da33bda51acb3b1
cal <calvinsoares@gmail.com>
2025-04-23 01:05:22 -0300

    feat: duplicate sheet

--- src/http/controllers/sheets/routes.ts
+++ src/http/controllers/sheets/routes.ts
@@ -5,11 +5,13 @@ import {
   updateSheet,
   deleteSheet,
   deleteAllSheets,
+  duplicateSheet,
 } from "./sheetsController";
 
 export async function sheetsRoutes(fastify: FastifyInstance) {
   fastify.get("/sheets/all", getSheets);
   fastify.post("/sheets", createSheet);
+  fastify.post("/sheets/:id/duplicate", duplicateSheet);
   fastify.patch("/sheets/:id", updateSheet);
   fastify.delete("/sheets/:id", deleteSheet);
   fastify.delete("/sheets/all", deleteAllSheets);
